5d4fdbd8e8a2ac4021a5c1d2de6ef801b5f1aa9d
Phil Ledoit <74996868+pledoit@users.noreply.github.com>
2021-11-02 14:56:23 -0400

    subbed in working app contents

--- app/main.py
+++
@@ -1,5 +0,0 @@
-from flask import Flask
-app=Flask(__name__)
-@app.route('/')
-def index():
-    return "<h1>Welcome to CodingX</h1>"
--- wsgi.py
+++
@@ -1,4 +0,0 @@
-
-from app.main import app
-if __name__ == "main":
-    app.run()
